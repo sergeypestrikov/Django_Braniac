a0d94ce0e722f784b6cc8ec3490bbafc1e2bbc2e
Sergey Pestrikov <sergey_pestrikov@mail.ru>
2023-03-24 13:09:46 +0300

    user authentication + social authentication via GitHub

--- authapp/urls.py
+++ authapp/urls.py
@@ -1,12 +1,12 @@
 from authapp.apps import AuthappConfig
 from django.urls import path
-from authapp.views import LoginView, RegisterView, LogoutView, EditView, CustomLoginView
+from authapp.views import LoginView, RegisterView, LogoutView, EditView, CustomLoginView, CustomLogoutView
 
 app_name = AuthappConfig.name
 
 urlpatterns = [
     path('login/', CustomLoginView.as_view(), name='login'),
     path('register/', RegisterView.as_view(), name='register'),
-    path('logout/', CustomLoginView.as_view(), name='logout'),
+    path('logout/', CustomLogoutView.as_view(), name='logout'),
     path('edit/', EditView.as_view(), name='edit'),
 ]
